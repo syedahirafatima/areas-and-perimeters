2bb11d734fd9c25f156a91a41269ef0ea8a248ac
Syeda Hira Fatima <syedahirafatima11@gmail.com>
2025-06-29 23:23:33 +0500

    square.py

---
+++ square.py
@@ -0,0 +1,12 @@
+# length of one side of the square
+side = float(input("Enter the length of the side of the square: "))
+
+# Calculate area
+area = side * side
+
+# Calculate perimeter
+perimeter = 4 * side
+
+# Output the results
+print("Area of the square is:", area)
+print("Perimeter of the square is:", perimeter)
