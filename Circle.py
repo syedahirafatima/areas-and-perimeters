5699ebd4c33aa53d6c932fb5c423b71daa3184b8
Syeda Hira Fatima <syedahirafatima11@gmail.com>
2025-07-26 18:09:09 +0500

    Circle.py

--- Circle.py
+++ Circle.py
@@ -7,4 +7,4 @@ radius = float(input("Enter the radius of the circle: "))
 area = math.pi * radius * radius
 
 # result
-print("The area of the circle is:", area)
+print("The area of the circle is :", area)
